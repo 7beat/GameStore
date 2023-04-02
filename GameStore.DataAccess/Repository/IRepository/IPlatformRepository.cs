a2cf23a085003b5d513e603a2c0d18ecf2b6984d
Tomasz Krzemiński <7beat123@wp.pl>
2023-04-02 19:27:22 +0200

    Added IPlatformRepository

---
+++ GameStore.DataAccess/Repository/IRepository/IPlatformRepository.cs
@@ -0,0 +1,9 @@
+﻿using GameStore.Models;
+
+namespace GameStore.DataAccess.Repository.IRepository
+{
+    public interface IPlatformRepository : IRepository<Platform>
+    {
+        void Update(Platform obj);
+    }
+}
