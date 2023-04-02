354501d2c9f7dc1d8bdeb971f5b12963a5fc5ff1
Tomasz Krzemiński <7beat123@wp.pl>
2023-04-02 19:28:48 +0200

    Sample Index

--- GameStoreWeb/Areas/Admin/Controllers/PlatformController.cs
+++ GameStoreWeb/Areas/Admin/Controllers/PlatformController.cs
@@ -1,13 +1,23 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using GameStore.DataAccess.Repository.IRepository;
+using GameStore.Models;
+using Microsoft.AspNetCore.Mvc;
 
 namespace GameStoreWeb.Areas.Admin.Controllers
 {
     [Area("Admin")]
     public class PlatformController : Controller
     {
+        private readonly IUnitOfWork _unitOfWork;
+
+        public PlatformController(IUnitOfWork unitOfWork)
+        {
+            _unitOfWork = unitOfWork;
+        }
+
         public IActionResult Index()
         {
-            return View();
+            var platform = _unitOfWork.Platform.GetFirstOrDefault(x => x.Id == 1); // GetAall
+            return View(platform);
         }
     }
 }
