e92abcde0fb59cdcf142dfb75279a8f08ecbcb26
Tomasz Krzemiński <7beat123@wp.pl>
2023-04-02 17:06:46 +0200

    Added admin controller

---
+++ GameStoreWeb/Areas/Admin/Controllers/PlatformController.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace GameStoreWeb.Areas.Admin.Controllers
+{
+    [Area("Admin")]
+    public class PlatformController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
