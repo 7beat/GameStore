125c6393b4a091e7671bea827ad5971ecc700dec
Tomasz Krzemiński <7beat123@wp.pl>
2023-04-02 19:27:49 +0200

    Added UnitOfWork with Platform

---
+++ GameStore.DataAccess/Repository/UnitOfWork.cs
@@ -0,0 +1,29 @@
+﻿using GameStore.DataAccess.Repository.IRepository;
+using GameStoreWeb.Data;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GameStore.DataAccess.Repository
+{
+    public class UnitOfWork : IUnitOfWork
+    {
+        public IPlatformRepository Platform { get; private set; }
+
+        private ApplicationDbContext _db;
+
+        public UnitOfWork(ApplicationDbContext db)
+        {
+            _db = db;
+            Platform = new PlatformRepository(_db);
+        }
+
+        public void Save()
+        {
+            _db.SaveChanges(); 
+            // Add in Program.cs
+        }
+    }
+}
