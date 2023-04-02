af32a423b9450847d41324df6acd6179420d15c2
Tomasz Krzemiński <7beat123@wp.pl>
2023-04-02 16:13:10 +0200

    Moved file to sub-folder

--- GameStore.DataAccess/Data/ApplicationDbContext.cs
+++ GameStore.DataAccess/Data/ApplicationDbContext.cs
@@ -1,4 +1,4 @@
-﻿using GameStore.Models;
+﻿using GameStore.Models.Identity;
 using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
 
--- GameStore.Models/Identity/ApplicationUser.cs
+++ GameStore.Models/Identity/ApplicationUser.cs
@@ -1,7 +1,7 @@
 ﻿using Microsoft.AspNetCore.Identity;
 using System.ComponentModel.DataAnnotations;
 
-namespace GameStore.Models
+namespace GameStore.Models.Identity
 {
     public class ApplicationUser : IdentityUser
     {
