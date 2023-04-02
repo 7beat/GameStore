c5036915dcebb3a56c6f9ffd16c9ec0228a2eb4c
Tomasz Krzemiński <7beat123@wp.pl>
2023-04-02 17:06:05 +0200

    Added basic IRepository

---
+++ GameStore.DataAccess/Repository/IRepository/IRepository.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GameStore.DataAccess.Repository.IRepository
+{
+    public interface IRepository<T> where T : class
+    {
+        T GetFirstOrDefault(Expression<Func<T, bool>> predicate, params string[] includeProperties);
+        void Add(T entity);
+        void Remove(T entity);
+    }
+}
