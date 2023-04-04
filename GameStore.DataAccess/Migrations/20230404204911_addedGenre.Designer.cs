5ed64dc69af60f5cb2faa8c20ab882763ed6cbf0
Tomasz Krzemiński <7beat123@wp.pl>
2023-04-04 23:05:57 +0200

    Migrations

--- GameStore.DataAccess/Migrations/20230404204911_addedGenre.Designer.cs
+++ GameStore.DataAccess/Migrations/20230404204911_addedGenre.Designer.cs
@@ -4,6 +4,7 @@ using GameStoreWeb.Data;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 #nullable disable
@@ -11,9 +12,10 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace GameStore.DataAccess.Migrations
 {
     [DbContext(typeof(ApplicationDbContext))]
-    partial class ApplicationDbContextModelSnapshot : ModelSnapshot
+    [Migration("20230404204911_addedGenre")]
+    partial class addedGenre
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
