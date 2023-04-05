fa50c5189c1489654b50b39ed1c765dd2d0b28b1
Tomasz Krzemiński <7beat123@wp.pl>
2023-04-05 19:36:00 +0200

    Implemented ProductRepository

---
+++ GameStore.DataAccess/Repository/ProductRepository.cs
@@ -0,0 +1,21 @@
+﻿using GameStore.DataAccess.Repository.IRepository;
+using GameStore.Models;
+using GameStoreWeb.Data;
+
+namespace GameStore.DataAccess.Repository
+{
+    public class ProductRepository : Repository<Product>, IProductRepository
+    {
+        private ApplicationDbContext _db;
+
+        public ProductRepository(ApplicationDbContext db) : base(db)
+        {
+            _db = db;
+        }
+
+        public void Update(Product obj)
+        {
+            _db.Products.Update(obj);
+        }
+    }
+}
