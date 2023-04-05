de0ff521b02203b9d257d6220bf566eab71b23be
Tomasz Krzemiński <7beat123@wp.pl>
2023-04-05 20:39:09 +0200

    Added Product View Model

---
+++ GameStore.Models/ViewModels/ProductVM.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Mvc.ModelBinding.Validation;
+using Microsoft.AspNetCore.Mvc.Rendering;
+
+namespace GameStore.Models.ViewModels
+{
+    public class ProductVM
+    {
+        public Product Product { get; set; }
+        [ValidateNever]
+        public IEnumerable<SelectListItem> GenreList { get; set; }
+        [ValidateNever]
+        public IEnumerable<SelectListItem> PlatformList { get; set; }
+    }
+}
