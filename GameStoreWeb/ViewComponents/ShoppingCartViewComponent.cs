dad932459d9c7159cae837b4e096ede0352c4ae1
Tomasz Krzemiński <7beat123@wp.pl>
2023-04-06 23:08:58 +0200

    Added Cart count logic

---
+++ GameStoreWeb/ViewComponents/ShoppingCartViewComponent.cs
@@ -0,0 +1,33 @@
+﻿using GameStoreWeb.Areas.Customer.Controllers;
+using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+
+namespace GameStoreWeb.ViewComponents
+{
+    public class ShoppingCartViewComponent : ViewComponent
+    {
+        public async Task<IViewComponentResult> InvokeAsync()
+        {
+            List<CartItem> cartItems = new List<CartItem>();
+            int totalQuantity = 0;
+
+            if (Request.Cookies.ContainsKey("ShoppingCart"))
+            {
+                string cartCookie = Request.Cookies["ShoppingCart"];
+                cartItems = JsonConvert.DeserializeObject<List<CartItem>>(cartCookie);
+
+                foreach (var item in cartItems)
+                {
+                    totalQuantity += item.Quantity;
+                }
+
+                return View(totalQuantity);
+            }
+            else
+            {
+                return View(0);
+            }
+        }
+    }
+}
+
