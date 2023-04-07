26400889a260d05145b274b5b19b0aeb2d600eb3
Tomasz Krzemiński <7beat123@wp.pl>
2023-04-07 17:25:42 +0200

    Added ICookieCart Repository

---
+++ GameStore.DataAccess/Repository/IRepository/ICookieShoppingCartRepository.cs
@@ -0,0 +1,11 @@
+﻿using GameStore.Models;
+
+namespace GameStore.DataAccess.Repository.IRepository
+{
+    public interface ICookieShoppingCartRepository
+    {
+        List<CartItem> GetAll();
+        void Add(CartItem item);
+        void Update(CartItem item);
+    }
+}
