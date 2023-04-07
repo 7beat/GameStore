a1f123f9c1790b3bdb1e9f3bec710474a7d57e75
Tomasz Krzemiński <7beat123@wp.pl>
2023-04-07 17:26:26 +0200

    Added Constructor for CookieCart

--- GameStore.DataAccess/Repository/UnitOfWork.cs
+++ GameStore.DataAccess/Repository/UnitOfWork.cs
@@ -1,5 +1,6 @@
 ﻿using GameStore.DataAccess.Repository.IRepository;
 using GameStoreWeb.Data;
+using Microsoft.AspNetCore.Http;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -13,6 +14,7 @@ namespace GameStore.DataAccess.Repository
         public IPlatformRepository Platform { get; private set; }
         public IGenreRepository Genre { get; private set; }
         public IProductRepository Product { get; private set; }
+        public ICookieShoppingCartRepository CookieShoppingCart { get; private set; }
 
         private ApplicationDbContext _db;
 
@@ -22,6 +24,12 @@ namespace GameStore.DataAccess.Repository
             Platform = new PlatformRepository(_db);
             Genre = new GenreRepository(_db);
             Product = new ProductRepository(_db);
+        }
+
+        public UnitOfWork(ApplicationDbContext db, IHttpContextAccessor httpContextAccessor)
+            : this(db) // Call the other constructor to initialize common dependencies
+        {
+            CookieShoppingCart = new CookieShoppingCartRepository(httpContextAccessor);
         }
 
         public void Save()
