f5bc42bb6d032337e471ae2bbdfef20bf84137f7
Tomasz Krzemiński <7beat123@wp.pl>
2023-04-07 12:45:31 +0200

    Added Utility Project

---
+++ GameStore.Utility/AppConsts.cs
@@ -0,0 +1,7 @@
+﻿namespace GameStore.Utility
+{
+    public static class AppConsts
+    {
+        public const string CookieCart = "ShoppingCart";
+    }
+}
---
+++ GameStore.Utility/AppRoles.cs
@@ -0,0 +1,8 @@
+﻿namespace GameStore.Utility
+{
+    public static class AppRoles
+    {
+        public const string Admin = "Admin";
+        public const string User = "User";
+    }
+}
