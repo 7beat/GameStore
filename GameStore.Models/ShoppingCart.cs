a606ab0a37dd6c814c325452122e84840799e227
Tomasz Krzemiński <7beat123@wp.pl>
2023-04-09 01:35:25 +0200

    Added jsonSerialize on selected properties

--- GameStore.Models/ShoppingCart.cs
+++ GameStore.Models/ShoppingCart.cs
@@ -1,22 +1,21 @@
 ﻿using GameStore.Models.Identity;
 using Microsoft.AspNetCore.Mvc.ModelBinding.Validation;
-using System;
-using System.Collections.Generic;
-using System.ComponentModel.DataAnnotations.Schema;
+using Newtonsoft.Json;
 using System.ComponentModel.DataAnnotations;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations.Schema;
 
 namespace GameStore.Models
 {
+	[JsonObject(MemberSerialization.OptIn)]
 	public class ShoppingCart
 	{
 		public int Id { get; set; }
+		[JsonProperty]
 		public int ProductId { get; set; }
 		[ForeignKey("ProductId")]
 		[ValidateNever]
 		public Product Product { get; set; }
+		[JsonProperty]
 		[Range(1, 1000, ErrorMessage = "Please enter a value between 1 and 1000")]
 		public int Count { get; set; }
 
