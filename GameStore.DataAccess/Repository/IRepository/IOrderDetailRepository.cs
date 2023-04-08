b8eb38f8bb4832304cad4988383385920e109064
Tomasz Krzemiński <7beat123@wp.pl>
2023-04-08 16:42:16 +0200

    Added IOrderDetailRepository

---
+++ GameStore.DataAccess/Repository/IRepository/IOrderDetailRepository.cs
@@ -0,0 +1,10 @@
+﻿using GameStore.DataAccess.Repository.IRepository;
+using GameStore.Models;
+
+namespace BookStore.DataAccess.Repository.IRepository
+{
+	public interface IOrderDetailRepository : IRepository<OrderDetail>
+	{
+		void Update(OrderDetail obj);
+	}
+}
