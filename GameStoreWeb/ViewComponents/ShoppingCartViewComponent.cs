aa3db5fb017285c4f8751973aec2413ab64d7ece
Tomasz Krzemiński <7beat123@wp.pl>
2023-04-08 18:18:07 +0200

    Added Concept Db Cart count

--- GameStoreWeb/ViewComponents/ShoppingCartViewComponent.cs
+++ GameStoreWeb/ViewComponents/ShoppingCartViewComponent.cs
@@ -1,14 +1,40 @@
-﻿using GameStore.Models;
+﻿using GameStore.DataAccess.Repository.IRepository;
+using GameStore.Models;
 using GameStore.Utility;
 using Microsoft.AspNetCore.Mvc;
 using Newtonsoft.Json;
+using System.Security.Claims;
 
 namespace GameStoreWeb.ViewComponents
 {
     public class ShoppingCartViewComponent : ViewComponent
     {
-        public async Task<IViewComponentResult> InvokeAsync()
+		private readonly IUnitOfWork _unitOfWork;
+
+		public ShoppingCartViewComponent(IUnitOfWork unitOfWork)
+		{
+			_unitOfWork = unitOfWork;
+		}
+
+		public async Task<IViewComponentResult> InvokeAsync()
         {
+            if (User.Identity.IsAuthenticated)
+            {
+				var claimsIdentity = (ClaimsIdentity)User.Identity;
+				var claim = claimsIdentity.FindFirst(ClaimTypes.NameIdentifier);
+
+                var cartItems = await _unitOfWork.ShoppingCart.GetAllAsync(x => x.ApplicationUserId == claim.Value);
+
+                int totalQuantity = 0;
+
+                foreach (var item in cartItems)
+                {
+                    totalQuantity += item.Count;
+                }
+
+                return View(totalQuantity);
+            }
+
             if (Request.Cookies.ContainsKey(AppConsts.CookieCart))
             {
                 string cartCookie = Request.Cookies[AppConsts.CookieCart];
