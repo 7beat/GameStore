5bd06743c6894a3f1ce0a3de589683ebe460626d
Tomasz Krzemiński <7beat123@wp.pl>
2023-04-08 16:43:07 +0200

    Implemented Repositories

---
+++ GameStore.DataAccess/Repository/OrderDetailRepository.cs
@@ -0,0 +1,22 @@
+﻿using BookStore.DataAccess.Repository.IRepository;
+using GameStore.DataAccess.Repository;
+using GameStore.Models;
+using GameStoreWeb.Data;
+
+namespace BookStore.DataAccess.Repository
+{
+	public class OrderDetailRepository : Repository<OrderDetail>, IOrderDetailRepository
+	{
+		private ApplicationDbContext _db;
+
+		public OrderDetailRepository(ApplicationDbContext db) : base(db)
+		{
+			_db = db;
+		}
+
+		public void Update(OrderDetail obj)
+		{
+			_db.OrderDetails.Update(obj);
+		}
+	}
+}
