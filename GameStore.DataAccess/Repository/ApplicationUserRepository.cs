8e6ab507cd0e16b603cf475b6484a6976cfcabf2
Tomasz Krzemiński <7beat123@wp.pl>
2023-04-08 16:50:11 +0200

    Implemented IApplicationUserRepository

---
+++ GameStore.DataAccess/Repository/ApplicationUserRepository.cs
@@ -0,0 +1,18 @@
+﻿using BookStore.DataAccess.Repository.IRepository;
+using GameStore.DataAccess.Repository;
+using GameStore.Models.Identity;
+using GameStoreWeb.Data;
+
+namespace BookStore.DataAccess.Repository
+{
+	public class ApplicationUserRepository : Repository<ApplicationUser>, IApplicationUserRepository
+	{
+		private ApplicationDbContext _db;
+
+		public ApplicationUserRepository(ApplicationDbContext db) : base(db)
+		{
+			_db = db;
+		}
+
+	}
+}
