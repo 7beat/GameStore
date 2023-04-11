242814510cbf27a864776ab30dba2662bc38d077
Tomasz Krzemiński <7beat123@wp.pl>
2023-04-11 14:59:49 +0200

    Concept Game Codes sending

--- GameStoreWeb/Areas/Customer/Controllers/CartController.cs
+++ GameStoreWeb/Areas/Customer/Controllers/CartController.cs
@@ -1,39 +1,37 @@
-﻿using GameStore.DataAccess.Repository;
-using GameStore.DataAccess.Repository.IRepository;
+﻿using GameStore.DataAccess.Repository.IRepository;
 using GameStore.Models;
 using GameStore.Models.ViewModels;
 using GameStore.Utility;
-using GameStoreWeb.Models;
+using Microsoft.AspNetCore.Identity.UI.Services;
 using Microsoft.AspNetCore.Mvc;
-using Newtonsoft.Json;
-using Stripe;
 using Stripe.Checkout;
-using System.Diagnostics;
 using System.Security.Claims;
 
 namespace GameStoreWeb.Areas.Customer.Controllers
 {
-    [Area("Customer")]
-    public class CartController : Controller
-    {
-        private readonly ILogger<CartController> _logger;
-        private readonly IUnitOfWork _unitOfWork;
+	[Area("Customer")]
+	public class CartController : Controller
+	{
+		private readonly ILogger<CartController> _logger;
+		private readonly IUnitOfWork _unitOfWork;
 		private readonly ICookieShoppingCartRepository _cookieCartRepository;
+		private readonly IEmailSender _emailSender;
 
 		[BindProperty]
 		public ShoppingCartVM ShoppingCartVM { get; set; }
 
-		public CartController(ILogger<CartController> logger, IUnitOfWork unitOfWork, ICookieShoppingCartRepository cookieCartRepository)
-        {
-            _logger = logger;
-            _unitOfWork = unitOfWork;
+		public CartController(ILogger<CartController> logger, IUnitOfWork unitOfWork, ICookieShoppingCartRepository cookieCartRepository, IEmailSender emailSender)
+		{
+			_logger = logger;
+			_unitOfWork = unitOfWork;
 			_cookieCartRepository = cookieCartRepository;
-        }
+			_emailSender = emailSender;
+		}
 
-        public async Task<IActionResult> Index()
-        {
-            if (User.Identity.IsAuthenticated)
-            {
+		public async Task<IActionResult> Index()
+		{
+			if (User.Identity.IsAuthenticated)
+			{
 				var userId = User.FindFirstValue(ClaimTypes.NameIdentifier);
 
 				ShoppingCartVM = new ShoppingCartVM()
@@ -58,192 +56,207 @@ namespace GameStoreWeb.Areas.Customer.Controllers
 					ShoppingCartVM.OrderHeader.OrderTotal += (cart.Product.Price * cart.Count);
 				}
 			}
-            return View(ShoppingCartVM);
-        }
+			return View(ShoppingCartVM);
+		}
 
-        public async Task<IActionResult> Summary()
-        {
+		public async Task<IActionResult> Summary()
+		{
 			if (User.Identity.IsAuthenticated)
 			{
-                var userId = User.FindFirstValue(ClaimTypes.NameIdentifier);
+				var userId = User.FindFirstValue(ClaimTypes.NameIdentifier);
 
-                ShoppingCartVM = new ShoppingCartVM()
-                {
-                    ListCart = await _unitOfWork.ShoppingCart.GetAllAsync(x => x.ApplicationUserId == userId, "Product", "ApplicationUser"),
-                    OrderHeader = new()
-                };
+				ShoppingCartVM = new ShoppingCartVM()
+				{
+					ListCart = await _unitOfWork.ShoppingCart.GetAllAsync(x => x.ApplicationUserId == userId, "Product", "ApplicationUser"),
+					OrderHeader = new()
+				};
 
-                ShoppingCartVM.OrderHeader.ApplicationUser = _unitOfWork.ApplicationUser.GetFirstOrDefault(x => x.Id == userId);
+				ShoppingCartVM.OrderHeader.ApplicationUser = _unitOfWork.ApplicationUser.GetFirstOrDefault(x => x.Id == userId);
 
-                ShoppingCartVM.OrderHeader.Name = ShoppingCartVM.OrderHeader.ApplicationUser.UserName;
-                ShoppingCartVM.OrderHeader.PhoneNumber = ShoppingCartVM.OrderHeader.ApplicationUser.PhoneNumber;
-                ShoppingCartVM.OrderHeader.StreetAddress = ShoppingCartVM.OrderHeader.ApplicationUser.StreetAddress;
-                ShoppingCartVM.OrderHeader.City = ShoppingCartVM.OrderHeader.ApplicationUser.City;
-                ShoppingCartVM.OrderHeader.State = ShoppingCartVM.OrderHeader.ApplicationUser.State;
-                ShoppingCartVM.OrderHeader.PostalCode = ShoppingCartVM.OrderHeader.ApplicationUser.PostalCode;
+				ShoppingCartVM.OrderHeader.Name = ShoppingCartVM.OrderHeader.ApplicationUser.UserName;
+				ShoppingCartVM.OrderHeader.PhoneNumber = ShoppingCartVM.OrderHeader.ApplicationUser.PhoneNumber;
+				ShoppingCartVM.OrderHeader.StreetAddress = ShoppingCartVM.OrderHeader.ApplicationUser.StreetAddress;
+				ShoppingCartVM.OrderHeader.City = ShoppingCartVM.OrderHeader.ApplicationUser.City;
+				ShoppingCartVM.OrderHeader.State = ShoppingCartVM.OrderHeader.ApplicationUser.State;
+				ShoppingCartVM.OrderHeader.PostalCode = ShoppingCartVM.OrderHeader.ApplicationUser.PostalCode;
 
-                foreach (var cart in ShoppingCartVM.ListCart)
-                {
-                    ShoppingCartVM.OrderHeader.OrderTotal += (cart.Product.Price * cart.Count);
-                }
+				foreach (var cart in ShoppingCartVM.ListCart)
+				{
+					ShoppingCartVM.OrderHeader.OrderTotal += (cart.Product.Price * cart.Count);
+				}
 			}
 			else
 			{
-                ShoppingCartVM = new()
-                {
-                    ListCart = GetCookieCartProducts(),
-                    OrderHeader = new()
-                };
-                foreach (var cart in ShoppingCartVM.ListCart)
-                {
-                    ShoppingCartVM.OrderHeader.OrderTotal += (cart.Product.Price * cart.Count);
-                }
-            }
+				ShoppingCartVM = new()
+				{
+					ListCart = GetCookieCartProducts(),
+					OrderHeader = new()
+				};
+				foreach (var cart in ShoppingCartVM.ListCart)
+				{
+					ShoppingCartVM.OrderHeader.OrderTotal += (cart.Product.Price * cart.Count);
+				}
+			}
 
-            return View(ShoppingCartVM);
-        }
+			return View(ShoppingCartVM);
+		}
 
-        [HttpPost]
+		[HttpPost]
 		[ActionName(nameof(Summary))]
 		[ValidateAntiForgeryToken]
-        public async Task<IActionResult> SummaryPost()
-        {
-            if (User.Identity.IsAuthenticated)
-            {
-                var userId = User.FindFirstValue(ClaimTypes.NameIdentifier);
+		public async Task<IActionResult> SummaryPost()
+		{
+			if (User.Identity.IsAuthenticated)
+			{
+				var userId = User.FindFirstValue(ClaimTypes.NameIdentifier);
 
-                ShoppingCartVM.ListCart = await _unitOfWork.ShoppingCart.GetAllAsync(x => x.ApplicationUserId == userId, "Product");
+				ShoppingCartVM.ListCart = await _unitOfWork.ShoppingCart.GetAllAsync(x => x.ApplicationUserId == userId, "Product");
 
-                ShoppingCartVM.OrderHeader.OrderDate = DateTime.Now;
-                ShoppingCartVM.OrderHeader.ApplicationUserId = userId;
+				ShoppingCartVM.OrderHeader.OrderDate = DateTime.Now;
+				ShoppingCartVM.OrderHeader.ApplicationUserId = userId;
 
-                foreach (var cart in ShoppingCartVM.ListCart)
-                {
-                    cart.Price = (cart.Product.Price * cart.Count);
-                    ShoppingCartVM.OrderHeader.OrderTotal += (cart.Price * cart.Count);
-                }
-            }
-            else
-            {
-                ShoppingCartVM.ListCart = GetCookieCartProducts();
+				foreach (var cart in ShoppingCartVM.ListCart)
+				{
+					cart.Price = (cart.Product.Price * cart.Count);
+					ShoppingCartVM.OrderHeader.OrderTotal += (cart.Price * cart.Count);
+				}
+			}
+			else
+			{
+				ShoppingCartVM.ListCart = GetCookieCartProducts();
 
-                ShoppingCartVM.OrderHeader.OrderDate = DateTime.Now;
-                ShoppingCartVM.OrderHeader.Name = string.Empty;
+				ShoppingCartVM.OrderHeader.OrderDate = DateTime.Now;
+				ShoppingCartVM.OrderHeader.Name = string.Empty;
 
-                foreach (var cart in ShoppingCartVM.ListCart)
-                {
-                    cart.Price = (cart.Product.Price * cart.Count);
-                    ShoppingCartVM.OrderHeader.OrderTotal += (cart.Price * cart.Count);
-                }
-            }
+				foreach (var cart in ShoppingCartVM.ListCart)
+				{
+					cart.Price = (cart.Product.Price * cart.Count);
+					ShoppingCartVM.OrderHeader.OrderTotal += (cart.Price * cart.Count);
+				}
+			}
 
 			ShoppingCartVM.OrderHeader.PaymentStatus = AppConsts.PaymentStatusPending;
 			ShoppingCartVM.OrderHeader.OrderStatus = AppConsts.StatusPending;
 
-            _unitOfWork.OrderHeader.Add(ShoppingCartVM.OrderHeader);
-            await _unitOfWork.SaveAsync();
+			_unitOfWork.OrderHeader.Add(ShoppingCartVM.OrderHeader);
+			await _unitOfWork.SaveAsync();
 
-            foreach (var cart in ShoppingCartVM.ListCart)
-            {
-                OrderDetail orderDetail = new()
-                {
-                    ProductId = cart.ProductId, // Product.Id
-                    OrderId = ShoppingCartVM.OrderHeader.Id,
-                    Price = cart.Price,
-                    Count = cart.Count
-                };
-                _unitOfWork.OrderDetail.Add(orderDetail);
-                await _unitOfWork.SaveAsync();
-            }
+			foreach (var cart in ShoppingCartVM.ListCart)
+			{
+				OrderDetail orderDetail = new()
+				{
+					ProductId = cart.ProductId, // Product.Id
+					OrderId = ShoppingCartVM.OrderHeader.Id,
+					Price = cart.Price,
+					Count = cart.Count
+				};
+				_unitOfWork.OrderDetail.Add(orderDetail);
+				await _unitOfWork.SaveAsync();
+			}
 
-            var domain = $"{Request.Scheme}://{Request.Host}/";
-            var options = new SessionCreateOptions
-            {
-                LineItems = new List<SessionLineItemOptions>(),
-                Mode = "payment",
-                SuccessUrl = domain + $"customer/cart/OrderConfirmation?id={ShoppingCartVM.OrderHeader.Id}",
-                CancelUrl = domain + $"customer/cart/index",
-            };
+			var domain = $"{Request.Scheme}://{Request.Host}/";
+			var options = new SessionCreateOptions
+			{
+				LineItems = new List<SessionLineItemOptions>(),
+				Mode = "payment",
+				SuccessUrl = domain + $"customer/cart/OrderConfirmation?id={ShoppingCartVM.OrderHeader.Id}",
+				CancelUrl = domain + $"customer/cart/index",
+			};
 
-            foreach (var item in ShoppingCartVM.ListCart)
-            {
-                var sessionLineItem = new SessionLineItemOptions
-                {
-                    PriceData = new SessionLineItemPriceDataOptions
-                    {
-                        UnitAmount = (long)(item.Price * 100),
-                        Currency = "usd",
-                        ProductData = new SessionLineItemPriceDataProductDataOptions
-                        {
-                            Name = item.Product.Title,
-                        },
-                    },
-                    Quantity = item.Count,
-                };
-                options.LineItems.Add(sessionLineItem);
-            }
+			foreach (var item in ShoppingCartVM.ListCart)
+			{
+				var sessionLineItem = new SessionLineItemOptions
+				{
+					PriceData = new SessionLineItemPriceDataOptions
+					{
+						UnitAmount = (long)(item.Price * 100),
+						Currency = "usd",
+						ProductData = new SessionLineItemPriceDataProductDataOptions
+						{
+							Name = item.Product.Title,
+						},
+					},
+					Quantity = item.Count,
+				};
+				options.LineItems.Add(sessionLineItem);
+			}
 
-            var service = new SessionService();
-            Session session = service.Create(options);
+			var service = new SessionService();
+			Session session = service.Create(options);
 
-            _unitOfWork.OrderHeader.UpdateStripePaymentID(ShoppingCartVM.OrderHeader.Id, session.Id, session.PaymentIntentId);
-            _unitOfWork.Save();
+			_unitOfWork.OrderHeader.UpdateStripePaymentID(ShoppingCartVM.OrderHeader.Id, session.Id, session.PaymentIntentId);
+			_unitOfWork.Save();
 
-            Response.Headers.Add("Location", session.Url);
-            return new StatusCodeResult(303);
-        }
+			Response.Headers.Add("Location", session.Url);
+			return new StatusCodeResult(303);
+		}
 
-        public async Task<IActionResult> OrderConfirmation(int id)
-        {
-            OrderHeader orderHeader = _unitOfWork.OrderHeader.GetFirstOrDefault(x => x.Id == id, includeProperties: "ApplicationUser");
-            
-            var service = new SessionService();
-            Session session = service.Get(orderHeader.SessionId);
-
-            if (session.PaymentStatus.ToLower() == "paid")
-            {
-                _unitOfWork.OrderHeader.UpdateStripePaymentID(id, orderHeader.SessionId, session.PaymentIntentId);
-                _unitOfWork.OrderHeader.UpdateStatus(id, AppConsts.StatusApproved, AppConsts.PaymentStatusApproved);
-                await _unitOfWork.SaveAsync();
-            }
-
-            // Send email
-            //_emailSender.SendEmailAsync(orderHeader.ApplicationUser.Email, "New Order - GameStore", "<p>New Order Created</p>");
-
-            if (User.Identity.IsAuthenticated)
-            {
-                var shoppingCarts = await _unitOfWork.ShoppingCart.GetAllAsync(x => x.ApplicationUserId == orderHeader.ApplicationUserId);
-
-                _unitOfWork.ShoppingCart.RemoveRange(shoppingCarts);
-                await _unitOfWork.SaveAsync();
-            }
-            else
-            {
-                _cookieCartRepository.RemoveCart();
-            }
-
-            return View(id);
-        }
-
-        public async Task<IActionResult> Plus(int cartId)
+		public async Task<IActionResult> OrderConfirmation(int id)
 		{
-            if (User.Identity.IsAuthenticated)
-            {
+			OrderHeader orderHeader = _unitOfWork.OrderHeader.GetFirstOrDefault(x => x.Id == id, includeProperties: "ApplicationUser");
+
+			var service = new SessionService();
+			Session session = service.Get(orderHeader.SessionId);
+
+			if (session.PaymentStatus.ToLower() == "paid")
+			{
+				_unitOfWork.OrderHeader.UpdateStripePaymentID(id, orderHeader.SessionId, session.PaymentIntentId);
+				_unitOfWork.OrderHeader.UpdateStatus(id, AppConsts.StatusApproved, AppConsts.PaymentStatusApproved);
+				await _unitOfWork.SaveAsync();
+			}
+
+			// Send email
+			//_emailSender.SendEmailAsync(orderHeader.ApplicationUser.Email, "New Order - GameStore", "<p>New Order Created</p>");
+
+			if (User.Identity.IsAuthenticated)
+			{
+				var shoppingCarts = await _unitOfWork.ShoppingCart.GetAllAsync(x => x.ApplicationUserId == orderHeader.ApplicationUserId);
+
+				_unitOfWork.ShoppingCart.RemoveRange(shoppingCarts);
+				await _unitOfWork.SaveAsync();
+			}
+			else
+			{
+				if (ShoppingCartVM.OrderHeader.IsDigital)
+				{
+					string productKeys = string.Empty;
+					foreach (var item in ShoppingCartVM.ListCart)
+					{
+						productKeys += $"{item.Product.Title} - {Guid.NewGuid()}"; 
+					}
+
+					await _emailSender.SendEmailAsync(orderHeader.GuestEmailAddress, "New Order - GameStore", 
+						 @$"<p>New Order Created</p>
+									<b>Your games:</b> <br />
+									<ul>{productKeys}</ul>");
+
+					_unitOfWork.OrderHeader.UpdateStatus(id, AppConsts.StatusShipped);
+				}
+				_cookieCartRepository.RemoveCart();
+			}
+
+			return View(id);
+		}
+
+		public async Task<IActionResult> Plus(int cartId)
+		{
+			if (User.Identity.IsAuthenticated)
+			{
 				var cart = await _unitOfWork.ShoppingCart.GetFirstOrDefaultAsync(x => x.Id == cartId);
 				_unitOfWork.ShoppingCart.IncrementCount(cart, 1);
 				await _unitOfWork.SaveAsync();
 			}
 			else
-            {
-                var cookieCart = _cookieCartRepository.GetAll().Where(x => x.ProductId == cartId).SingleOrDefault();
-                cookieCart.Count++;
-                _cookieCartRepository.Update(cookieCart);
-            }
+			{
+				var cookieCart = _cookieCartRepository.GetAll().Where(x => x.ProductId == cartId).SingleOrDefault();
+				cookieCart.Count++;
+				_cookieCartRepository.Update(cookieCart);
+			}
 			return RedirectToAction(nameof(Index));
 		}
 
 		public async Task<IActionResult> Minus(int cartId)
-        {
+		{
 			if (User.Identity.IsAuthenticated)
 			{
 				var cart = await _unitOfWork.ShoppingCart.GetFirstOrDefaultAsync(x => x.Id == cartId);
@@ -293,8 +306,8 @@ namespace GameStoreWeb.Areas.Customer.Controllers
 			{
 				shoppingCarts.Add(new()
 				{
-                    Product = _unitOfWork.Product.GetFirstOrDefault(x => x.Id == item.ProductId),
-                    ProductId = item.ProductId,
+					Product = _unitOfWork.Product.GetFirstOrDefault(x => x.Id == item.ProductId),
+					ProductId = item.ProductId,
 					Count = item.Count,
 				});
 			}
