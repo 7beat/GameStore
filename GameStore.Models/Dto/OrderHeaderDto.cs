6f822d2acbe9d9add98424e34dacc866d2e3f277
Tomasz Krzemiński <7beat123@wp.pl>
2023-05-02 17:29:44 +0200

    Added OrderHeaderDto with proper validation

---
+++ GameStore.Models/Dto/OrderHeaderDto.cs
@@ -0,0 +1,29 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace GameStore.Models.Dto
+{
+    public class OrderHeaderDto
+    {
+        public double OrderTotal { get; set; }
+
+        [Required]
+        [EmailAddress]
+        public string? GuestEmailAddress { get; set; }
+        [Required]
+        [StringLength(13, ErrorMessage = "The {0} must be at least {2} and at max {1} characters long.", MinimumLength = 10)]
+        public string? PhoneNumber { get; set; }
+        [Required]
+        public string? StreetAddress { get; set; }
+        [Required]
+        public string? City { get; set; }
+        [Required]
+        public string? State { get; set; }
+        [Required]
+        public string? PostalCode { get; set; }
+        [Required]
+        public string? Name { get; set; }
+
+        public bool IsDigital { get; set; } = true;
+    }
+}
+
